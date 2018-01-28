b4ad2a582eef732f2b368c882f1789d67bd83ff3
Rajiv Sahu <rajivsahu10@gmail.com>
2018-01-28 20:16:43 +0530

    corrected source file names in json output and few other bugs
    corrected source file names in json output and few other bugs

--- gitlog2json.sh
+++ gitlog2json.sh
@@ -4,7 +4,6 @@
 baseDir="$(pwd)"                                                                                                                                                                       
 repoDir='/home/rajiv/gitNinja/sampleRepos/spring-data-keyvalue-examples'
 commitsJsonFile="${baseDir}/commits.json"
-#cmd_gitlog=' git log --remotes=origin --numstat --pretty=oneline -2 --format="AuthorName:%aN%nAuthorEmail:%aE%nAuthorDate:%aI"'
 echo "RepoDir=${repoDir}"
 echo "baseDir=${baseDir}"
 
@@ -14,8 +13,9 @@ addCurrentCommit(){
     #params=$1
     #diffStats=$2
     #commitCount=$3
-    echo " commit count = $commitCount"
-    echo "commit log path = $commitsJsonFile"
+    
+    #echo " commit count = $commitCount"
+    #echo "commit log path = $commitsJsonFile"
      
     if [ " $commitCount" -gt 1 ]; then
         printf ", \n{ " >> $commitsJsonFile
@@ -23,7 +23,7 @@ addCurrentCommit(){
         printf "\n{ " >> $commitsJsonFile 
     fi
 
-    firstEntry=0
+    #firstEntry=0
     for entryLine in "${currentCommitParams[@]}"
     do
        #echo "Functin loop 1 : $entryLine"
@@ -31,26 +31,32 @@ addCurrentCommit(){
        #echo "key[$key]"
        value=$( echo $entryLine | sed 's/^[^:]*://' | sed 's/\"//g' )
        #echo "value[$value]"
-       if [ "$firstEntry" -eq 0 ]; then
-           ((++firstEntry))
-           printf "\n\t\"$key\" : \"$value\"" >>  $commitsJsonFile
-       else
-           printf ",\n\t\"$key\" : \"$value\"" >> $commitsJsonFile
-       fi
+       
+       printf "\n\t\"$key\" : \"$value\"," >>  $commitsJsonFile
+        
+       ## don't need the below block anymore as the diffStats entry
+       ## is always going to be there, even when the diff is empty
+       ## as it is an array
+       #if [ "$firstEntry" -eq 0 ]; then
+       #    ((++firstEntry))
+       #    printf "\n\t\"$key\" : \"$value\"" >>  $commitsJsonFile
+       #else
+       #    printf ",\n\t\"$key\" : \"$value\"" >> $commitsJsonFile
+       #fi
     done
    
     firstEntry=0
     printf "\n\t\"diffStats\" : [ " >> $commitsJsonFile
     for diffEntry in "${currentCommitDiffStats[@]}"
     do 
-        echo "digit : $diffEntry"
+        #echo "digit : $diffEntry"
         locAdded=$(echo "$diffEntry" | cut -d$'\t' -f1)
         #echo "locAdded[$locAdded]"
         locDeleted=$(echo "$diffEntry" | cut -d$'\t' -f2)
         #echo "locDeleted[$locDeleted]"
-        srcFile=$( echo "$diffEntry" | cut -d$'\t' -f3 | sed 's/\"//g' )
+        srcFile=$( echo "$diffEntry" | cut -d$'\t' -f3 | sed 's/\"//g' | sed 's/^[^\/]*\///' | sed 's/\//./g' )
         #echo "srcFile[$srcFile]"
-        srcFileType=$(echo "$srcFile" | sed 's/.*\.//')
+        srcFileType=$(echo "$srcFile" | sed 's/.*\.//' )
         #echo "srcFileType[$srcFileType]"  
         
         
@@ -81,15 +87,14 @@ addCurrentCommit(){
 cd $repoDir
 echo "Changed dir to $(pwd)"
 
-#echo "command to execute : $cmd_gitlog"
-#logOutput="`$cmd_gitlog`"
-
-
+#### clean up before execution (precautionary) ########
 rm -f temp.log
 rm -f $commitsJsonFile
-git log --remotes=origin --numstat --pretty=oneline --date=short -2 --format="AuthorName:%aN%nAuthorEmail:%aE%nAuthorDate:%ad%nhash:%h%nSubject:%s" | grep -v -e ^[[:space:]]*$ > temp.log
 
-commitsList=()
+####### running git log command ########
+git log --remotes=origin --numstat --pretty=oneline --date=short -2 --format="authorName:%aN%nauthorEmail:%aE%nauthorDate:%ad%nhash:%H%nsubject:%s" | grep -v -e ^[[:space:]]*$ > temp.log
+
+############### START: processing of git log output #############################
 currentCommitParams=()
 currentCommitDiffStats=()
 commitCount=0
@@ -102,7 +107,7 @@ do
     if [[ $line =~ ^[[:alpha:]] ]]; then
       #echo "alpha : $line"
       key=$( echo $line | sed 's/:.*//' | sed 's/\"//g' )
-      if [ "$key" == "AuthorName" ]; then
+      if [ "$key" == "authorName" ]; then
           if [ "$commitCount" -gt 0 ]; then
               addCurrentCommit $currentCommitParams $currentCommitDiffStats $commitCount
               currentCommitParams=()
@@ -115,13 +120,20 @@ do
     elif [[ $line =~ ^[[:digit:]] ]]; then
       currentCommitDiffStats+=("$line")
     else
-      echo "@Rajiv : invalid line"
+      ###### ignoring these entries #######
+      echo "@Rajiv : invalid line" > /dev/null
     fi
 done < temp.log
 
+#####  special handling for the last commit entry ########
 addCurrentCommit $currentCommitParams $currentCommitDiffStats $commitCount
 
-# Removing temp.log file
+################## END: processing of git log out ###################################
+
+#######  displaying result info  ##########
+echo "Generated Json file : $commitsJsonFile"
+
+##### Removing temp.log file  #######
 rm -f temp.log
 
 
